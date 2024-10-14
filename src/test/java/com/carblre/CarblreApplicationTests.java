ed2b70695675af1d381765d9c0c5ec2a152852e3
BHbae <majinga15@naver.com>
2024-10-14 14:49:23 +0900

    first commit

---
+++ src/test/java/com/carblre/CarblreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carblre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarblreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
