7a35551a8126a8126cf4ca1fd9f0f8648e65934f
ddg999 <slowman918@gmail.com>
2024-10-30 17:50:58 +0900

    권한오류수정

--- src/main/java/com/carblre/config/AdminInterceptor.java
+++ src/main/java/com/carblre/config/AdminInterceptor.java
@@ -3,6 +3,8 @@ package com.carblre.config;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import com.carblre.dto.userdto.UserDTO;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -13,7 +15,12 @@ public class AdminInterceptor implements HandlerInterceptor {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		// 세션에서 사용자 역할 확인
-		String userRole = (String) request.getSession().getAttribute("role");
+		UserDTO principal = (UserDTO) request.getSession().getAttribute("principal");
+		if (principal == null) {
+			response.sendRedirect("/access-denied"); // 접근 거부 페이지로 리다이렉트
+			return false;
+		}
+		String userRole = principal.getRole();
 
 		// 관리자 페이지 접근 확인
 		if (request.getRequestURI().startsWith("/admin") && (userRole == null || !userRole.equals("admin"))) {
